d570ab3333d48cbe441e0eba1faa19de7dce8df7
JonyEpsilon <github@j-star.org>
2014-09-17 11:56:58 +0100

    Add dependencies for last commit :-(

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
 (defproject algebolic "0.1.0-SNAPSHOT"
   :description "A library for doing symbolic regression with algebraic manipulation."
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-kdtree "1.2.0" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-gorilla "0.3.4-SNAPSHOT"]]
   :jvm-opts ["-server" "-Xmx4g"])
