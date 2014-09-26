2bdde531bd056283e9318e673c92e8562d893d85
JonyEpsilon <github@j-star.org>
2014-09-26 16:39:50 +0100

    First stab at differentiation.

--- src/algebolic/expression/differentiation.clj
+++ src/algebolic/expression/differentiation.clj
@@ -2,4 +2,25 @@
 ;;;;
 ;;;; Not for distribution.
 
-(ns algebolic.expression.differentiation)
+(ns algebolic.expression.differentiation
+  "Symbolic differentiation of expressions."
+  (:require [algebolic.expression.core :as expression]))
+
+(declare d)
+
+(defn- diff-expr
+  [var expr]
+  (case (first expr)
+    :plus (expression/make-expression :plus (map (partial d var) (rest expr)))
+    :minus (expression/make-expression :minus (map (partial d var) (rest expr)))
+    :times [:plus [:times (d var (nth expr 1)) (nth expr 2)] [:times (nth expr 1) (d var (nth expr 2))]]
+    :sin [:times (d var (nth expr 1)) [:cos (nth expr 1)]]
+    :cos [:times (d var (nth expr 1)) [:times -1 [:sin (nth expr 1)]]]))
+
+(defn d
+  "Returns the partial derivative of `expr` with respect to `var`."
+  [var expr]
+  (cond
+    (expression/non-terminal? expr) (diff-expr var expr)
+    (number? expr) 0
+    (symbol? expr) (if (= var expr) 1 0)))
