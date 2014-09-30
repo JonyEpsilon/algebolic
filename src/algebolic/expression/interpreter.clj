23460fa4b3b4d6ff7c39dc2e1a6e812a25622925
JonyEpsilon <github@j-star.org>
2014-09-30 11:50:40 +0100

    Really disable pdiv.

--- src/algebolic/expression/interpreter.clj
+++ src/algebolic/expression/interpreter.clj
@@ -20,7 +20,7 @@
                                       :plus (+ (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
                                       :minus (- (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
                                       :times (* (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
-                                      :div (expression/pdiv (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
+;                                      :div (expression/pdiv (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
                                       :sin (Math/sin (evaluate (nth expr 1) vars))
                                       :cos (Math/cos (evaluate (nth expr 1) vars))
                                       (println "Failed: " expr " !!!"))
