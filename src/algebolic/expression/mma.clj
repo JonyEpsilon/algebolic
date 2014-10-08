1d776748a80a583315419ed5a42987c3d4e75cb5
JonyEpsilon <github@j-star.org>
2014-10-08 17:02:16 +0100

    Add export to Mathematica.

---
+++ src/algebolic/expression/mma.clj
@@ -0,0 +1,22 @@
+(ns algebolic.expression.mma
+  "Some functions for using Mathematica with algebolic expressions."
+  (:require [algebolic.expression.core :as expression]))
+
+(defn fullform
+  "Transform an algebolic expression to a string in Mathematica format. Useful for copying expressions
+  into Mathematica when you're stuck!"
+  [expr]
+  (cond
+    (expression/non-terminal? expr) (case (first expr)
+                                      :plus (str "Plus[" (fullform (nth expr 1)) ", "
+                                                 (fullform (nth expr 2)) "]")
+                                      :minus (str "Subtract[" (fullform (nth expr 1)) ", "
+                                                  (fullform (nth expr 2)) "]")
+                                      :times (str "Times[" (fullform (nth expr 1)) ", "
+                                                  (fullform (nth expr 2)) "]")
+                                      :sin (str "Sin[" (fullform (nth expr 1)) "]")
+                                      :cos (str "Cos[" (fullform (nth expr 1)) "]")
+                                      ;; make sure we notice if something has gone wrong
+                                      (println "Throw[\"Unknown expression: " expr "\"]"))
+    (symbol? expr) (str expr)
+    (number? expr) (str expr)))
