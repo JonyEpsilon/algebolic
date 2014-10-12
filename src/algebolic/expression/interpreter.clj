1e8173210cce15da77ff310df1620455796aa624
JonyEpsilon <github@j-star.org>
2014-10-12 21:59:59 +0100

    Go back to symbols and numbers in expressions.
    Experimenting shows that using 'instructions' for variables and
    constants isn't really any quicker, once you optimise the interpreter a
    bit.

--- src/algebolic/expression/interpreter.clj
+++ src/algebolic/expression/interpreter.clj
@@ -3,21 +3,22 @@
 ;;;; Not for distribution.
 
 (ns algebolic.expression.interpreter
-  "Provides an interpreter for algebolic expressions. This can be used to get the
-  value of an expression for particular values of the variables. It is much faster
-  for most use cases than compiling the expression to Clojure and evaluating it as
-  a function."
+  "Provides an interpreter for algebolic expressions. This can be used to get the value of an expression for particular
+  values of the variables. It is much faster for most use cases than compiling the expression with the  Clojure compiler
+  and evaluating it as a function."
   (:require [algebolic.expression.core :as expression]))
 
 (defn evaluate
-  "Evaluate the given expression at the given value of the variables."
+  "Evaluate the given expression at the given value of the variables. The variables should
+  be specified as a map from symbol to value i.e. `{'x 3 'y 4}`."
   [expr vars]
-    (case (nth expr 0)
-      :plus (+ (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
-      :minus (- (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
-      :times (* (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
-      :div (expression/pdiv (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
-      :sin (Math/sin (evaluate (nth expr 1) vars))
-      :cos (Math/cos (evaluate (nth expr 1) vars))
-      :const (nth expr 1)
-      :var (nth vars (nth expr 1))))
+  (cond
+    (symbol? expr) (expr vars)
+    (number? expr) expr
+    true (case (nth expr 0)
+           :plus (+ (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
+           :minus (- (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
+           :times (* (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
+           :div (expression/pdiv (evaluate (nth expr 1) vars) (evaluate (nth expr 2) vars))
+           :sin (Math/sin (evaluate (nth expr 1) vars))
+           :cos (Math/cos (evaluate (nth expr 1) vars)))))
--- ws/scratch/interpreter-test.clj
+++ ws/scratch/interpreter-test.clj
@@ -24,7 +24,7 @@
 ;; <=
 
 ;; @@
-(def expr [:plus [:times [:var 0] [:var 0]] [:var 0]])
+(def expr [:plus [:times 'x 'x] 'x])
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-var'>#&#x27;balmy-hurricane/expr</span>","value":"#'balmy-hurricane/expr"}
@@ -34,7 +34,7 @@
 expr
 ;; @@
 ;; =>
-;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:plus</span>","value":":plus"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:times</span>","value":":times"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:var</span>","value":":var"},{"type":"html","content":"<span class='clj-long'>0</span>","value":"0"}],"value":"[:var 0]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:var</span>","value":":var"},{"type":"html","content":"<span class='clj-long'>0</span>","value":"0"}],"value":"[:var 0]"}],"value":"[:times [:var 0] [:var 0]]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:var</span>","value":":var"},{"type":"html","content":"<span class='clj-long'>0</span>","value":"0"}],"value":"[:var 0]"}],"value":"[:plus [:times [:var 0] [:var 0]] [:var 0]]"}
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:plus</span>","value":":plus"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:times</span>","value":":times"},{"type":"html","content":"<span class='clj-symbol'>x</span>","value":"x"},{"type":"html","content":"<span class='clj-symbol'>x</span>","value":"x"}],"value":"[:times x x]"},{"type":"html","content":"<span class='clj-symbol'>x</span>","value":"x"}],"value":"[:plus [:times x x] x]"}
 ;; <=
 
 ;; @@
@@ -47,12 +47,15 @@ expr
 ;; @@
 (f 3)
 ;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-long'>12</span>","value":"12"}
+;; <=
 
 ;; @@
 (time (do (doall (repeatedly 100 #(evaluate/functionalise expr '[x]))) :done))
 ;; @@
 ;; ->
-;;; &quot;Elapsed time: 106.656 msecs&quot;
+;;; &quot;Elapsed time: 79.161 msecs&quot;
 ;;; 
 ;; <-
 ;; =>
@@ -63,7 +66,7 @@ expr
 (time (do (doall (repeatedly 100 #(f 3))) :done))
 ;; @@
 ;; ->
-;;; &quot;Elapsed time: 0.399 msecs&quot;
+;;; &quot;Elapsed time: 0.365 msecs&quot;
 ;;; 
 ;; <-
 ;; =>
@@ -73,16 +76,9 @@ expr
 ;; @@
 (time (do (doall (repeatedly 100000 #(interpreter/evaluate expr [3]))) :done))
 ;; @@
-;; ->
-;;; &quot;Elapsed time: 49.118 msecs&quot;
-;;; 
-;; <-
-;; =>
-;;; {"type":"html","content":"<span class='clj-keyword'>:done</span>","value":":done"}
-;; <=
 
 ;; @@
-(interpreter/evaluate expr [7])
+(interpreter/evaluate expr {'x 7})
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-long'>56</span>","value":"56"}
@@ -91,35 +87,30 @@ expr
 ;; @@
 (criterium/with-progress-reporting
   (criterium/quick-bench
-    (interpreter/evaluate expr [3.0])
+    (interpreter/evaluate expr {'x 3.0})
     :verbose))
 ;; @@
 ;; ->
 ;;; Warming up for JIT optimisations 5000000000 ...
-;;;   compilation occured before 210546 iterations
-;;;   compilation occured before 27157874 iterations
-;;;   compilation occured before 27368400 iterations
-;;;   compilation occured before 27578926 iterations
-;;;   compilation occured before 27789452 iterations
-;;;   compilation occured before 27999978 iterations
+;;;   compilation occured before 124237 iterations
 ;;; Estimating execution count ...
 ;;; Sampling ...
 ;;; Final GC...
 ;;; Checking GC...
-;;; WARNING: Final GC required 48.9934217651921 % of runtime
+;;; WARNING: Final GC required 51.87962624405742 % of runtime
 ;;; Finding outliers ...
 ;;; Bootstrapping ...
 ;;; Checking outlier significance
 ;;; x86_64 Mac OS X 10.9.5 4 cpu(s)
 ;;; Java HotSpot(TM) 64-Bit Server VM 25.5-b02
 ;;; Runtime arguments: -Dfile.encoding=UTF-8 -Xmx4g -Dclojure.compile.path=/Users/jony/Documents/Work/Computing/ProjectX/Projects/algebolic/target/classes -Dalgebolic.version=0.1.0-SNAPSHOT -Dclojure.debug=false
-;;; Evaluation count : 3881214 in 6 samples of 646869 calls.
-;;;       Execution time sample mean : 154.520584 ns
-;;;              Execution time mean : 154.514142 ns
-;;; Execution time sample std-deviation : 2.010962 ns
-;;;     Execution time std-deviation : 2.107593 ns
-;;;    Execution time lower quantile : 152.522755 ns ( 2.5%)
-;;;    Execution time upper quantile : 156.804920 ns (97.5%)
+;;; Evaluation count : 4054650 in 6 samples of 675775 calls.
+;;;       Execution time sample mean : 136.656481 ns
+;;;              Execution time mean : 136.492472 ns
+;;; Execution time sample std-deviation : 2.006614 ns
+;;;     Execution time std-deviation : 2.169094 ns
+;;;    Execution time lower quantile : 134.141097 ns ( 2.5%)
+;;;    Execution time upper quantile : 138.546410 ns (97.5%)
 ;;;                    Overhead used : 1.904185 ns
 ;;; 
 ;; <-
