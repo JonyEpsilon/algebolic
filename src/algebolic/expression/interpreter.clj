78cb732e9b92bd195c2ab0926358003e6a7c65ca
JonyEpsilon <github@j-star.org>
2014-10-12 17:44:49 +0100

    Decide on keyword based interpreter for the mo'.

--- src/algebolic/expression/interpreter.clj
+++ src/algebolic/expression/interpreter.clj
@@ -21,22 +21,3 @@
       :cos (Math/cos (evaluate (nth expr 1) vars))
       :const (nth expr 1)
       :var (nth vars (nth expr 1))))
-
-(defrecord Instruction [^Integer op ^Double arg1 ^Double arg2])
-
-(defn itr
-  ([op arg1] (->Instruction op arg1 0.0))
-  ([op arg1 arg2] (->Instruction op arg1 arg2)))
-
-(defn ^Double evaluate-rec
-  "Evaluate the given expression at the given value of the variables."
-  [^Instruction expr ^doubles vars]
-  (case (:op expr)
-    0 (+ (evaluate-rec (:arg1 expr) vars) (evaluate-rec (:arg2 expr) vars))
-    1 (- (evaluate-rec (:arg1 expr) vars) (evaluate-rec (:arg2 expr) vars))
-    2 (* (evaluate-rec (:arg1 expr) vars) (evaluate-rec (:arg2 expr) vars))
-    3 (expression/pdiv (evaluate-rec (:arg1 expr) vars) (evaluate-rec (:arg2 expr) vars))
-    4 (Math/sin (evaluate-rec (:arg1 expr) vars))
-    5 (Math/cos (evaluate-rec (:arg1 expr) vars))
-    6 (:arg1 expr)
-    7 (nth vars (:arg1 expr))))
