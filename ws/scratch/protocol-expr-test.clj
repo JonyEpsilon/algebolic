1ea885371358c8eadd91266a17e9a0df832be50d
JonyEpsilon <github@j-star.org>
2014-10-14 21:12:57 +0100

    More fiddling with protocols.

--- ws/scratch/protocol-expr-test.clj
+++ ws/scratch/protocol-expr-test.clj
@@ -11,7 +11,7 @@
 ;; **
 
 ;; @@
-(ns icy-winter2
+(ns icy-winter4
   (:require [gorilla-plot.core :as plot]
             [criterium.core :as criterium]))
 ;; @@
@@ -28,85 +28,101 @@
 
 ;; @@
 (defprotocol Eval
-  (evaluate [expr vars]))
+  (evaluate ^Double [expr ^Double vars]))
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-symbol'>Eval</span>","value":"Eval"}
 ;; <=
 
 ;; @@
-(defrecord Plus [a1 a2]
+(deftype Plus [a1 a2]
   Eval
   (evaluate [expr vars] (+ (evaluate a1 vars) (evaluate a2 vars))))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-class'>icy_winter2.Plus</span>","value":"icy_winter2.Plus"}
+;;; {"type":"html","content":"<span class='clj-class'>icy_winter4.Plus</span>","value":"icy_winter4.Plus"}
 ;; <=
 
 ;; @@
-(defrecord Times [a1 a2]
+(deftype Times [a1 a2]
   Eval
   (evaluate [expr vars] (* (evaluate a1 vars) (evaluate a2 vars))))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-class'>icy_winter2.Times</span>","value":"icy_winter2.Times"}
+;;; {"type":"html","content":"<span class='clj-class'>icy_winter4.Times</span>","value":"icy_winter4.Times"}
 ;; <=
 
 ;; @@
-(defrecord Const [c]
+(deftype Const [c]
   Eval
   (evaluate [expr vars] c))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-class'>icy_winter2.Const</span>","value":"icy_winter2.Const"}
+;;; {"type":"html","content":"<span class='clj-class'>icy_winter4.Const</span>","value":"icy_winter4.Const"}
 ;; <=
 
 ;; @@
-(def expr (->Plus (->Const 3.0) (->Const 4.0)))
+(deftype Var []
+  Eval
+  (evaluate [expr vars] vars))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;icy-winter2/expr</span>","value":"#'icy-winter2/expr"}
+;;; {"type":"html","content":"<span class='clj-class'>icy_winter4.Var</span>","value":"icy_winter4.Var"}
 ;; <=
 
 ;; @@
-(evaluate expr [])
+(def expr (->Plus (->Times (->Const 3.0) (->Const 4.0)) (->Const 5.0)))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-double'>7.0</span>","value":"7.0"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;icy-winter4/expr</span>","value":"#'icy-winter4/expr"}
+;; <=
+
+;; @@
+(evaluate expr 3.0)
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-double'>17.0</span>","value":"17.0"}
 ;; <=
 
 ;; @@
 (criterium/with-progress-reporting
   (criterium/quick-bench
-    (evaluate expr [])
+    (evaluate expr 3.0)
     :verbose))
 ;; @@
 ;; ->
 ;;; Warming up for JIT optimisations 5000000000 ...
-;;;   compilation occured before 289888 iterations
+;;;   compilation occured before 52746 iterations
 ;;; Estimating execution count ...
 ;;; Sampling ...
 ;;; Final GC...
 ;;; Checking GC...
-;;; WARNING: Final GC required 47.59581721710602 % of runtime
+;;; WARNING: Final GC required 45.81696803051546 % of runtime
 ;;; Finding outliers ...
 ;;; Bootstrapping ...
 ;;; Checking outlier significance
 ;;; x86_64 Mac OS X 10.9.5 4 cpu(s)
 ;;; Java HotSpot(TM) 64-Bit Server VM 25.5-b02
 ;;; Runtime arguments: -Dfile.encoding=UTF-8 -Xmx4g -Dclojure.compile.path=/Users/jony/Documents/Work/Computing/ProjectX/Projects/algebolic/target/classes -Dalgebolic.version=0.1.0-SNAPSHOT -Dclojure.debug=false
-;;; Evaluation count : 32073558 in 6 samples of 5345593 calls.
-;;;       Execution time sample mean : 17.061090 ns
-;;;              Execution time mean : 17.060622 ns
-;;; Execution time sample std-deviation : 0.272819 ns
-;;;     Execution time std-deviation : 0.285657 ns
-;;;    Execution time lower quantile : 16.784008 ns ( 2.5%)
-;;;    Execution time upper quantile : 17.325692 ns (97.5%)
+;;; Evaluation count : 15336132 in 6 samples of 2556022 calls.
+;;;       Execution time sample mean : 39.035511 ns
+;;;              Execution time mean : 38.981717 ns
+;;; Execution time sample std-deviation : 1.462412 ns
+;;;     Execution time std-deviation : 1.552678 ns
+;;;    Execution time lower quantile : 37.092453 ns ( 2.5%)
+;;;    Execution time upper quantile : 40.722459 ns (97.5%)
 ;;;                    Overhead used : 1.895866 ns
 ;;; 
 ;; <-
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
+
+;; @@
+Eval
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:on</span>","value":":on"},{"type":"html","content":"<span class='clj-symbol'>icy_winter4.Eval</span>","value":"icy_winter4.Eval"}],"value":"[:on icy_winter4.Eval]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:on-interface</span>","value":":on-interface"},{"type":"html","content":"<span class='clj-class'>icy_winter4.Eval</span>","value":"icy_winter4.Eval"}],"value":"[:on-interface icy_winter4.Eval]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:sigs</span>","value":":sigs"},{"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:evaluate</span>","value":":evaluate"},{"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:doc</span>","value":":doc"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}],"value":"[:doc nil]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:arglists</span>","value":":arglists"},{"type":"list-like","open":"<span class='clj-list'>(</span>","close":"<span class='clj-list'>)</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-symbol'>expr</span>","value":"expr"},{"type":"html","content":"<span class='clj-symbol'>vars</span>","value":"vars"}],"value":"[expr vars]"}],"value":"([expr vars])"}],"value":"[:arglists ([expr vars])]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:name</span>","value":":name"},{"type":"html","content":"<span class='clj-symbol'>evaluate</span>","value":"evaluate"}],"value":"[:name evaluate]"}],"value":"{:doc nil, :arglists ([expr vars]), :name evaluate}"}],"value":"[:evaluate {:doc nil, :arglists ([expr vars]), :name evaluate}]"}],"value":"{:evaluate {:doc nil, :arglists ([expr vars]), :name evaluate}}"}],"value":"[:sigs {:evaluate {:doc nil, :arglists ([expr vars]), :name evaluate}}]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:var</span>","value":":var"},{"type":"html","content":"<span class='clj-var'>#&#x27;icy-winter4/Eval</span>","value":"#'icy-winter4/Eval"}],"value":"[:var #'icy-winter4/Eval]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:method-map</span>","value":":method-map"},{"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:evaluate</span>","value":":evaluate"},{"type":"html","content":"<span class='clj-keyword'>:evaluate</span>","value":":evaluate"}],"value":"[:evaluate :evaluate]"}],"value":"{:evaluate :evaluate}"}],"value":"[:method-map {:evaluate :evaluate}]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:method-builders</span>","value":":method-builders"},{"type":"list-like","open":"<span class='clj-map'>{</span>","close":"<span class='clj-map'>}</span>","separator":", ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-var'>#&#x27;icy-winter4/evaluate</span>","value":"#'icy-winter4/evaluate"},{"type":"html","content":"<span class='clj-unkown'>#&lt;icy_winter4$eval7596$fn__7597 icy_winter4$eval7596$fn__7597@197683f3&gt;</span>","value":"#<icy_winter4$eval7596$fn__7597 icy_winter4$eval7596$fn__7597@197683f3>"}],"value":"[#'icy-winter4/evaluate #<icy_winter4$eval7596$fn__7597 icy_winter4$eval7596$fn__7597@197683f3>]"}],"value":"{#'icy-winter4/evaluate #<icy_winter4$eval7596$fn__7597 icy_winter4$eval7596$fn__7597@197683f3>}"}],"value":"[:method-builders {#'icy-winter4/evaluate #<icy_winter4$eval7596$fn__7597 icy_winter4$eval7596$fn__7597@197683f3>}]"}],"value":"{:on icy_winter4.Eval, :on-interface icy_winter4.Eval, :sigs {:evaluate {:doc nil, :arglists ([expr vars]), :name evaluate}}, :var #'icy-winter4/Eval, :method-map {:evaluate :evaluate}, :method-builders {#'icy-winter4/evaluate #<icy_winter4$eval7596$fn__7597 icy_winter4$eval7596$fn__7597@197683f3>}}"}
 ;; <=
 
 ;; @@
