2872b2df9dc199f0c95deeb2cd85e50a2323e08a
JonyEpsilon <github@j-star.org>
2015-07-16 13:52:37 +0100

    Generate 1.7 compatible JARs.

--- project.clj
+++ project.clj
@@ -8,14 +8,15 @@
 ; Released under the MIT license..
 ;
 
-(defproject algebolic "1.0.0"
+(defproject algebolic "1.0.1"
             :url "https://github.com/JonyEpsilon/algebolic"
             :license {:name "MIT"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [darwin "1.0.0"]
+                           [darwin "1.0.1"]
                            [clj-kdtree "1.2.0" :exclusions [org.clojure/clojure]]
                            [gorilla-renderable "2.0.0"]
                            [criterium "0.4.3"]]
+            :javac-options ["-target" "1.7" "-source" "1.7"]
             :java-source-paths ["java"]
             :plugins [[lein-gorilla "0.3.4"]]
             :jvm-opts ^:replace ["-server" "-Xmx4g"])
